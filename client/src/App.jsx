a5ef0789a6c90533297a807f2862ac74018558af
Dan Moffat <danmoffat@gmail.com>
2020-06-09 21:52:40 +0100

    Added basic routing to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Gallery from './components/pages/Gallery/Gallery';
+import './App.scss';
 
 const App = (props) => {
-  return <h1>App works!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/gallery" component={Gallery} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
